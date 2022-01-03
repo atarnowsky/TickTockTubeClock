055cc6c6a0b91e33e02af5d1ecc3128cadd71a5e
Andreas Tarnowsky <andreas.tarnowsky@atsd.net>
2022-01-03 17:50:11 +0100

    Use custom DS3232 interface to save 1K Flash, 32B Ram

---
+++ firmware/include/ds3232.h
@@ -0,0 +1,106 @@
+#pragma once
+#include <Wire.h>
+// This is an absolute minimal interface to the RS3232M RTC IC.
+// For generale usage, I highly recommend a properly implemented
+// library like Jack Christensen's DS3232RTC Library
+// @see https://github.com/JChristensen/DS3232RTC
+// For this project, we do not need any advanced features like
+// calendar and alarm functions. We only use:
+//  - Set/get current hours and minutes
+//  - Get current temperature
+//  - Get oscillator status
+// Basically, this is a stripped down version of the aforementioned library.
+
+namespace DS3232M {
+    enum class Status : uint8_t {
+        OKAY,
+        I2C_ERROR,
+        OSCILLATOR_FAILURE
+    };
+
+    struct time_t {
+        int8_t hours;
+        int8_t minutes;
+    };
+
+    namespace util {
+        inline uint8_t decode_bcd(uint8_t n) {
+            return n - 6 * (n >> 4);
+        }
+
+        inline uint8_t to_bcd(uint8_t n) {
+            return n + 6 * (n / 10);
+        }
+
+        uint8_t read(uint8_t reg) {
+            Wire.beginTransmission(0x68);
+            Wire.write(reg);
+            Wire.endTransmission();
+            Wire.requestFrom(0x68, 1);
+            return Wire.read();
+        }
+
+        uint8_t write(uint8_t reg, uint8_t value) {
+            Wire.beginTransmission(0x68);
+            Wire.write(reg);
+            Wire.write(value);
+            return Wire.endTransmission();
+        }
+    }
+
+    using temperature_t = uint8_t;
+
+    inline Status initialize() {
+        Wire.begin();
+
+        // Set control register
+        if(util::write(0x0E, 0b00100100) != 0){
+            return Status::I2C_ERROR;   
+        }
+
+        // Set status register
+        uint8_t stat_reg = util::read(0x0F);
+        util::write(0x0F, 0b00000000);
+
+        if((stat_reg & (1 << 7)) > 0) {
+            return Status::OSCILLATOR_FAILURE;
+        }
+        else {
+            return Status::OKAY;
+        }
+    }
+
+    inline temperature_t temperature() {
+        // We do not need the fractional part...
+        return util::read(0x11);
+    }
+
+    inline time_t time() {
+        Wire.beginTransmission(0x68);
+        Wire.write(0x00);
+        uint8_t error = Wire.endTransmission();
+        if(error) {
+            return {0, 0};
+        }
+        Wire.requestFrom(0x68, 7);
+        time_t result;
+        (void)Wire.read(); // Ignore seconds
+        result.minutes = util::decode_bcd(Wire.read());
+        result.hours = util::decode_bcd(Wire.read() & ~(1 << 6));
+        // Ignore rest of query
+        for(uint8_t i = 0; i < 4; i++)
+            Wire.read();
+        return result;
+    }
+
+    inline void set_time(const time_t& time) {
+        Wire.beginTransmission(0x68);
+        Wire.write(0x00);
+        Wire.write(0); // seconds
+        Wire.write(util::to_bcd(time.minutes)); 
+        Wire.write(util::to_bcd(time.hours)); 
+        for(uint8_t i = 0; i < 4; i++)
+            Wire.write(0);   
+    }
+
+}
--- firmware/src/timing.cpp
+++ firmware/src/timing.cpp
@@ -1,6 +1,12 @@
 #include "timing.h"
 
-#include <DS3232RTC.h>
+#define USE_INTERNAL_LIB
+
+#ifdef USE_INTERNAL_LIB
+    #include "ds3232.h"
+#else
+    #include <DS3232RTC.h>
+#endif
 
 // TODO: Using the RTC library is pretty (memory-) inefficient
 // since we are not using calendar functions at all.
@@ -9,32 +15,42 @@
 // implicit syncing mechanisms...
 
 namespace {
-    void set_time(int8_t minutes, int8_t hours) {
-        // We do not use any calendar functions so we
-        // can save some bytes of flash by NOT using all these TimeLib method.
-        // Let's assume we live in the year 1970:
-        time_t t = minutes * 60UL + hours * 3600UL;
-        RTC.set(t);
-        setTime(t);
+    void set_time(int8_t minutes, int8_t hours) {      
+
+        #ifdef USE_INTERNAL_LIB
+            DS3232M::set_time({hours, minutes});
+        #else
+            // We do not use any calendar functions so we
+            // can save some bytes of flash by NOT using all these TimeLib method.
+            // Let's assume we live in the year 1970:
+            time_t t = minutes * 60UL + hours * 3600UL;
+            RTC.set(t);
+            setTime(t);            
+        #endif
     }
 }
 
 void RTCSync::initialize() {
-    bool needs_init = RTC.oscStopped(true);
-    setSyncProvider(RTC.get);
-    if (timeStatus() != timeSet) {
-        // Show error number '0000'
-        return;
-    }
+    #ifdef USE_INTERNAL_LIB
+        if(DS3232M::initialize() == DS3232M::Status::OSCILLATOR_FAILURE)
+            DS3232M::set_time({0, 0});        
+    #else
+        bool needs_init = RTC.oscStopped(true);
+        setSyncProvider(RTC.get);
+        if (timeStatus() != timeSet) {
+            // Show error number '0000'
+            return;
+        }
 
-    if(needs_init) {   
-        set_time(0, 0);
-    }  
+        if(needs_init) {   
+            set_time(0, 0);
+        }  
 
-    setSyncInterval(10);
+        setSyncInterval(10);
 
-    // Disable unused RTC features
-    RTC.squareWave(SQWAVE_NONE);
+        // Disable unused RTC features
+        RTC.squareWave(SQWAVE_NONE);
+    #endif    
 }
 
 void RTCSync::process() {
@@ -42,17 +58,28 @@ void RTCSync::process() {
 }
 
 uint16_t RTCSync::current_time() {
-    time_t t = now();  
-    int8_t minutes = minute(t);
-    int16_t hours = hour(t);
-    return hours * 100 + minutes;
+    #ifdef USE_INTERNAL_LIB
+        DS3232M::time_t t = DS3232M::time();
+        return int16_t(t.hours)*100 + t.minutes;
+    #else
+        time_t t = now();  
+        int8_t minutes = minute(t);
+        int16_t hours = hour(t);
+        return hours * 100 + minutes;
+    #endif    
 }
 
 
 void RTCSync::increment_minutes(int8_t amount) {
-    time_t t = now();  
-    int8_t minutes = minute(t) + amount;  
-    int8_t hours = hour(t);
+    #ifdef USE_INTERNAL_LIB
+        DS3232M::time_t t = DS3232M::time();
+        int8_t minutes = t.minutes + amount;
+        int8_t hours = t.hours;
+    #else
+        time_t t = now();  
+        int8_t minutes = minute(t) + amount;  
+        int8_t hours = hour(t);
+    #endif
     
     // Normalize time
     if(minutes > 59) {
@@ -75,9 +102,15 @@ void RTCSync::increment_minutes(int8_t amount) {
 }
 
 void RTCSync::increment_hours(int8_t amount) {
-    time_t t = now();  
-    int8_t minutes = minute(t);  
-    int8_t hours = hour(t) + amount;  
+    #ifdef USE_INTERNAL_LIB
+        DS3232M::time_t t = DS3232M::time();
+        int8_t minutes = t.minutes;
+        int8_t hours = t.hours + amount;
+    #else
+        time_t t = now();  
+        int8_t minutes = minute(t);  
+        int8_t hours = hour(t) + amount;
+    #endif
     
     // Normalize time
     if(hours > 23)
